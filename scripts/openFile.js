b4e362f5f5e76aba13ce44a22572fd08b55c4e33
BasilThaddeus <basiltqsi@gmail.com>
2022-12-31 05:23:20 -0800

    Created a basic template for projects, resume, and more. scripts/drag.js needs to be updated to support multiple divs along with only controlling the 'content-controls' div and not the entire container. Must move all scripts and css into public folder. Taskbar needs to be corrected as well.

---
+++ scripts/openFile.js
@@ -0,0 +1,18 @@
+let resume = document.getElementById('resume-container');
+let projects = document.getElementById('projects-container');
+
+function openResume() {
+    resume.style.visibility = "visible";
+}
+
+function hideResume() {
+    resume.style.visibility = "hidden";
+}
+
+function openProjects() {
+    projects.style.visibility = "visible";
+}
+
+function hideProjects() {
+    projects.style.visibility = "hidden";
+}
